b9203ebabe6496e18347e641b45f77a60dc7d93d
Katarina Kromar <kromar.katarina@gmail.com>
2015-12-02 11:55:51 +0100

    dodana druga funkcija - še vedno ne dela prav

--- uvoz/uvozpodatkov.r
+++ uvoz/uvozpodatkov.r
@@ -2,9 +2,15 @@
 
 # Funkcija, ki uvozi podatke iz datotek podatki umrljivost.csv in podatki cepljenost.csv
 uvozi.umrljivost <- function(){
-  return(read.table("podatki umrljivost.csv", sep=",", as.is=TRUE,
-                    col.names = c("Country", "Year", "Number of under-five deaths", "Number of infant deaths"),
+  return(read.table("podatki umrljivost.csv", sep=",",
+                    header=TRUE,
                     fileEncoding = "UTF-8"))
 }
 umrljivost <- uvozi.umrljivost()
 
+# read.csv(file= "podatki umrljivost.csv")
+# stolpci <- c("Država","Leto","Smrtnost do 5.leta starosti", "Smrtnost dojenčkov")
+# podatki <- read.csv2(file = "podatki umrljivost.csv", skip=1, 
+#+                      nrow=(195-1), header=FALSE, strip.white=TRUE, col.names=stolpci, 
+#+                      fileEncoding="Windows-1250")
+#
