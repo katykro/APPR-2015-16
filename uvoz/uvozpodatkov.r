b488045f7939830bcffac701c69d085412424623
Katarina Kromar <kromar.katarina@gmail.com>
2015-12-11 18:33:00 +0100

    Dokončana funkcija za uvoz podatkov iz csv

--- uvoz/uvozpodatkov.r
+++ uvoz/uvozpodatkov.r
@@ -5,33 +5,30 @@ require(dplyr)
 
 stolpci <- c("Drzava","Leto","Smrtnost do 5.leta starosti", "Smrtnost dojenckov")
 uvozi.umrljivost <- function(){
-  return(read.table("podatki-umrljivost.csv", sep=",",
-                    quote="",as.is = TRUE,
+  return(read.table("podatki/podatki-umrljivost.csv", sep=",",
+                    as.is = TRUE,
                     nrow=(195-1),header=FALSE,strip.white=TRUE, col.names=stolpci,na.strings="0 [0-0]",
                     fileEncoding = "UTF-8",skip=1))
 }
 umrljivost <- uvozi.umrljivost()
 umrljivost$Leto <- NULL
+umrljivost$Smrtnost.do.5.leta.starosti <- umrljivost$Smrtnost.do.5.leta.starosti %>% gsub("^([0-9. ]+).*", "\\1", .) %>% gsub(" ","", .) %>% as.numeric()
+umrljivost$Smrtnost.dojenckov <- umrljivost$Smrtnost.dojenckov %>% gsub("^([0-9. ]+).*", "\\1", .) %>% gsub(" ","", .) %>% as.numeric()
+umrljivost <- umrljivost[!is.na(umrljivost$Smrtnost.do.5.leta.starosti),]
+umrljivost <- umrljivost[!is.na(umrljivost$Smrtnost.dojenckov),]
 
-
+# cepljenost
 stolpci <- c("Drzava","Leto","k","Cepljenost-Z","Cepljenost-M")
 uvozi.cepljenost <- function(){
-  return(read.table("podatki-cepljenost.csv", sep=",",quote="",as.is = NA,
+  return(read.table("podatki/podatki-cepljenost.csv", sep=",",as.is = TRUE,
                     nrow=(65-2),header=FALSE,strip.white=TRUE, col.names=stolpci,na.strings="No data",
                     fileEncoding = "UTF-8",skip=2))
 }
 cepljenost <- uvozi.cepljenost()
 cepljenost$k <- NULL
 cepljenost$Leto <- NULL
-z <-strsplit(cepljenost$Cepljenost.Z, split = " ")[[1]]
-as.numeric(z[1])
-m <-strsplit(cepljenost$Cepljenost.M, split = " ")[[1]]
-as.numeric(m[1])
-#cepljenost$Cepljenost.Z <- as.numeric(cepljenost$Cepljenost.Z)
-#cepljenost$Cepljenost.M <- as.numeric(cepljenost$Cepljenost.M)
-cepljenost$Cepljenost.Z[cepljenost$Cepljenost.Z == "No data"] <- NA
 
-z <-strsplit("91.7 [83.5-96.0]", split = " ")[[1]]
-as.numeric(z[1])
-
-
+cepljenost$Cepljenost.Z <- cepljenost$Cepljenost.Z %>% gsub("^([0-9. ]+).*", "\\1", .) %>% gsub(" ","", .) %>% as.numeric()
+cepljenost$Cepljenost.M <- cepljenost$Cepljenost.M %>% gsub("^([0-9. ]+).*", "\\1", .) %>% gsub(" ","", .) %>% as.numeric()
+cepljenost <- cepljenost[!is.na(cepljenost$Cepljenost.Z),]
+cepljenost <- cepljenost[!is.na(cepljenost$Cepljenost.M),]
