407731daf661518a53183652a6770809acc15808
Katarina Kromar <kromar.katarina@gmail.com>
2015-12-13 14:57:42 +0100

    podatki xml

--- uvoz/uvozpodatkov.r
+++ uvoz/uvozpodatkov.r
@@ -1,6 +1,6 @@
 # Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datotek podatki umrljivost.csv in podatki cepljenost.csv
+## Funkcija, ki uvozi podatke iz datotek podatki-umrljivost.csv in podatki-cepljenost.csv
 require(dplyr)
 
 stolpci <- c("Drzava","Leto","Smrtnost do 5.leta starosti", "Smrtnost dojenckov")
@@ -16,6 +16,7 @@ umrljivost$Smrtnost.do.5.leta.starosti <- umrljivost$Smrtnost.do.5.leta.starosti
 umrljivost$Smrtnost.dojenckov <- umrljivost$Smrtnost.dojenckov %>% gsub("^([0-9. ]+).*", "\\1", .) %>% gsub(" ","", .) %>% as.numeric()
 umrljivost <- umrljivost[!is.na(umrljivost$Smrtnost.do.5.leta.starosti),]
 umrljivost <- umrljivost[!is.na(umrljivost$Smrtnost.dojenckov),]
+write.csv(umrljivost,"umrljivost.csv",row.names=FALSE)
 
 # cepljenost
 stolpci <- c("Drzava","Leto","k","Cepljenost-Z","Cepljenost-M")
@@ -32,6 +33,31 @@ cepljenost$Cepljenost.Z <- cepljenost$Cepljenost.Z %>% gsub("^([0-9. ]+).*", "\\
 cepljenost$Cepljenost.M <- cepljenost$Cepljenost.M %>% gsub("^([0-9. ]+).*", "\\1", .) %>% gsub(" ","", .) %>% as.numeric()
 cepljenost <- cepljenost[!is.na(cepljenost$Cepljenost.Z),]
 cepljenost <- cepljenost[!is.na(cepljenost$Cepljenost.M),]
+write.csv(cepljenost,"cepljenost.csv",row.names=FALSE)
 
 # združitev obeh tabel
 zdruzena <- full_join(umrljivost, cepljenost, "Drzava"="Drzava", copy=TRUE)
+write.csv(zdruzena, "zdruzeni.csv",row.names=FALSE)
+
+## Funkcija, ki uvozi podatke iz datotek podatki podhranjenost.txt in podatki nerazvitost.txt
+
+require(dplyr)
+require(httr)
+require(jsonlite)
+library(rjson)
+library(RCurl)
+
+html <- "http://apps.who.int/gho/data/view.main.90200"
+k <- htmlTreeParse(html,encoding = "UTF-8", useInternal = TRUE)
+tab <- read_html(html)
+tabela <- html_node(tab,xpath="//table") %>% .[[1]] %>% html_table(fill = TRUE)
+
+nerazvitost <- fromJSON(file="podatki/data (12).xml", method="C")
+r <- GET("http://apps.who.int/gho/athena/data/GHO/MDG_0000000027.json?filter=COUNTRY:*;REGION:*;SEX:*")
+text <- content(r, "text")
+data <- fromJSON(content(r, "text"))
+data$dataset %>% names() %>% print()
+
+tabela <- data.frame(data$dataset$data, stringsAsFactors=FALSE)
+names(tabela) <- data$dataset$column_name
+print(sapply(tabela, class))
