8a945ec5fe95e47aa3502611e3989dbbc86b6a82
Katarina Kromar <kromar.katarina@gmail.com>
2016-01-19 18:01:48 +0100

    zemljevida

--- lib/uvoz.zemljevid.r
+++ lib/uvoz.zemljevid.r
@@ -5,6 +5,7 @@ library(digest)
 gpclibPermit()
 library(ggplot2)
 library(dplyr)
+source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
 
 pretvori.zemljevid <- function(zemljevid,pogoj=TRUE) {
   fo <- fortify(zemljevid[pogoj,])
@@ -15,23 +16,26 @@ pretvori.zemljevid <- function(zemljevid,pogoj=TRUE) {
 
 svet<-uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip",
                       "ne_110m_admin_0_countries",force=FALSE)
-cep <- select(tidy_analiza, Drzava, Spol, Cepljenost)
-m <- match(svet$name_long,cep$Drzava)
-svet$Cepljenost <- tidy_analiza$Cepljenost[m]
-svet$Spol <- tidy_analiza$Spol[m]
-afrika <- pretvori.zemljevid(svet, svet$continent == "Africa")
-map1 <- ggplot() + geom_polygon(data = afrika, aes(x=long,y=lat, group=group,
-                                                   fill= Cepljenost), color="grey35")+ xlab("") + ylab("") +
-  scale_fill_gradient(low = "#132B43", high = "#56B1F7") +
-  guides(fill = guide_colorbar(title = "Cepljenost v odstotkih v Afriki"))
+podh <- select(vsi_zdruzeni, Drzava,`Odstotek podhranjenih otrok`)
+m <- match(svet$name_long,podh$Drzava)
+svet$Podhranjenost <- vsi_zdruzeni$`Odstotek podhranjenih otrok`[m]
+svet1 <- pretvori.zemljevid(svet)
+map1 <- ggplot() + geom_polygon(data = svet1, aes(x=long,y=lat, group=group,
+                                                   fill= Podhranjenost), color="grey35")+ xlab("") + ylab("") +
+  scale_fill_gradient(low = "#56B1F7", high = "#132B43") +
+  guides(fill = guide_colorbar(title = "Odstotek podhranjenih otrok - svet"))
 print(map1)
 
-smrt <- select(analiza, Drzava, Smrtnost.do.5.leta.starosti)
-m1 <- match(svet$name_long, smrt$Drzava)
-svet$Smrtnost <- analiza$Smrtnost.do.5.leta.starosti[m1]
-svet1 <- pretvori.zemljevid(svet)
-map2 <- ggplot() + geom_polygon(data = svet1, aes(x=long,y=lat, group=group,
-                                                   fill= Smrtnost), color="grey35")+ xlab("") + ylab("") +
-  scale_fill_gradient(low = "#132B43", high = "#56B1F7") +
-  guides(fill = guide_colorbar(title = "Smrtnost do 5. leta starosti"))
+raz <- select(vsi_zdruzeni, Drzava,`Odstotek nerazvitih otrok`)
+m2 <- match(svet$name_long, raz$Drzava)
+svet$Nerazvitost <- vsi_zdruzeni$`Odstotek nerazvitih otrok`[m2]
+afrika <- pretvori.zemljevid(svet, svet$continent == "Africa")
+map2 <- ggplot() + geom_polygon(data = afrika, aes(x=long,y=lat, group=group,
+                                                   fill= Nerazvitost), color="grey35")+ xlab("") + ylab("") +
+  scale_fill_gradient(low = "#56B1F7", high = "#132B43") +
+  guides(fill = guide_colorbar(title = "Odstotek nerazvitih otrok - Afrika"))
 print(map2)
+
+# zem3 <- map2 + geom_text(data = vsi_zdruzeni %>% filter(`Odstotek nerazvitih otrok` > 40),
+#                          aes(x=long, y=lat, label = Drzava),size = 3, vjust = 2)
+# print(zem3)
